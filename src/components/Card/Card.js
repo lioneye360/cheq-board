ee8c01c9ea18b96f2b60a3be2995303d43dfba8c
usher360 <lioneye360@gmail.com>
2021-04-30 18:15:47 +0300

    add cover image, edit menu

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,8 +1,24 @@
 import React from "react";
 import { Draggable } from "react-beautiful-dnd";
-import {Grid, IconButton, Modal, Paper} from "@material-ui/core";
+import {
+    Button,
+    Dialog, DialogActions,
+    DialogContent,
+    DialogTitle,
+    Fab,
+    Grid,
+    IconButton,
+    List,
+    ListItem,
+    Modal,
+    Paper
+} from "@material-ui/core";
 import useStyles from "./Card.style";
-import DeleteIcon from '@material-ui/icons/Delete';
+import DeleteIcon from '@material-ui/icons/Delete'
+import InsertPhotoIcon from '@material-ui/icons/InsertPhoto'
+import MenuIcon from '@material-ui/icons/Menu';
+import EditIcon from '@material-ui/icons/Edit';
+import AddIcon from "@material-ui/icons/Add";
 
 const grid = 8;
 
@@ -37,6 +53,22 @@ function Card({item, index, onClickDelete, onCardChange}) {
         onCardChange(newColumn);
     }
 
+    const handleFileChange = (e) => {
+        let file = e.target.files[0];
+        let fReader = new FileReader();
+
+        fReader.onload = function() {
+            // let imageElement = document.getElementById(`card-image-${item.id}`);
+            // imageElement.src = fReader.result;
+
+            //fReader.result.split(/base64,\//)[1]
+            let newColumn = {...item};
+            newColumn.image = fReader.result;
+            onCardChange(newColumn);
+        };
+        fReader.readAsDataURL(file);
+    };
+
   return (
       <Draggable
           draggableId={item.id}
@@ -52,34 +84,65 @@ function Card({item, index, onClickDelete, onCardChange}) {
                         provided.draggableProps.style
                     )}>
 
+                  <div className={classes.cardContent}>
+                      <Fab size="small" aria-label="delete" className={classes.deleteButton}>
+                          <DeleteIcon onClick={onClickDelete}/>
+                      </Fab>
 
-                  <Grid container spacing={0} alignItems={"center"}>
-                      <Grid item xs={9}>
-                          <h3 style={{'fontSize': '16px', 'fontWeight': '400', textAlign: 'left' }} onClick={handleOpen}>{item.title}</h3>
+                      <Fab size="small" aria-label="add" className={classes.editButton}>
+                          <EditIcon onClick={handleOpen}/>
+                      </Fab>
+
+                      <Grid container spacing={1} alignItems={"center"}>
+                          <Grid item xs={12}>
+                              { item.image && <img id={`card-image-${item.id}`} src={item.image} style={{'width': '100%', 'height': '100%'}} alt="card-image"/> }
+                          </Grid>
+                          <Grid item xs={9}>
+                              <h3 style={{'fontSize': '16px', 'fontWeight': '400', textAlign: 'left' }}>{item.title}</h3>
+                          </Grid>
+
                       </Grid>
-                      <Grid item xs={3}>
-                          <IconButton
-                              type="button"
-                              onClick={onClickDelete}
-                          >
-                              <DeleteIcon/>
-                          </IconButton>
-                      </Grid>
+                  </div>
 
-                  </Grid>
-
-                  <Modal
+                  <Dialog
                       open={open}
                       onClose={handleClose}
-                      className={classes.modal}
-                      aria-labelledby="modal-title"
-                      aria-describedby="modal-description"
+                      aria-labelledby="dialog-title"
+                      aria-describedby="dialog-description"
+                      fullWidth={true}
                   >
-                      <textarea
-                          className={classes.modalContent}
-                          defaultValue={item.title}
-                          onBlur={handleTitleChange}/>
-                  </Modal>
+                    <DialogTitle id="dialog-title">Card Edit</DialogTitle>
+
+                    <DialogContent>
+                        <Grid container spacing={1} alignItems={"center"} classes={{root: classes.cardEdit}}>
+                            <Grid item xs={6}>
+                              <label htmlFor={`file-upload-${item.id}`} className={classes.customFileUpload}>
+                                  Change Cover Image
+                                  <div className="MuiButtonBase-root MuiIconButton-root">
+                                      <InsertPhotoIcon/>
+                                  </div>
+                              </label>
+                              <input id={`file-upload-${item.id}`} type="file" accept="image/*" onChange={handleFileChange}/>
+                            </Grid>
+
+                            <Grid item xs={4}>
+                                { item.image && <img id={`card-image-${item.id}`} src={item.image} style={{'max-height': '90px'}} alt="card-image"/> }
+                            </Grid>
+                            <Grid item xs={12}>
+                                <textarea
+                                 className={classes.modalContent}
+                                 defaultValue={item.title}
+                                 onBlur={handleTitleChange}/>
+                            </Grid>
+                        </Grid>
+                        <DialogActions>
+                            <Button onClick={handleClose} color="primary">
+                                Close
+                            </Button>
+                        </DialogActions>
+                    </DialogContent>
+                  </Dialog >
+
               </Paper>
 
           )}
--- src/components/Card/Card.style.js
+++ src/components/Card/Card.style.js
@@ -8,6 +8,7 @@ const useStyles = makeStyles((theme) => ({
     padding: theme.spacing(2),
     textAlign: 'center',
     color: theme.palette.text.secondary,
+    position: 'relative'
   },
 
   modal: {
@@ -17,14 +18,65 @@ const useStyles = makeStyles((theme) => ({
   },
 
   modalContent: {
-    position: 'absolute',
+    position: 'relative',
     width: 400,
     backgroundColor: theme.palette.background.paper,
     border: '2px solid #000',
     boxShadow: theme.shadows[5],
     padding: theme.spacing(2, 4, 3),
     resize: 'none'
-  }
+  },
+
+  cardContent: {
+    '&:hover': {
+      '& $editButton': {
+        display: 'inline-flex'
+      },
+      '& $deleteButton': {
+        display: 'inline-flex'
+      }
+    },
+    '&:active': {
+      '& $editButton': {
+        display: 'inline-flex'
+      },
+      '& $deleteButton': {
+        display: 'inline-flex'
+      }
+    }
+  },
+
+  editButton: {
+    position: 'absolute',
+    top: '2px',
+    right: '4px',
+    display: 'none',
+    opacity: '0.9'
+  },
+
+  deleteButton: {
+    position: 'absolute',
+    top: '48px',
+    right: '4px',
+    display: 'none',
+    opacity: '0.9'
+  },
+
+  cardEdit: {
+    '& input[type="file"]': {
+      display: 'none'
+    }
+  },
+
+  customFileUpload: {
+    border: '1px solid #ccc',
+    display: 'inline-block',
+    padding: '6px 12px',
+    cursor: 'pointer'
+  },
+
+
+
 }));
 
 export default useStyles
